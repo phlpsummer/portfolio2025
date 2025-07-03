803500b9cd11efe04370bedbdf38bfef448ab748
김선형 <ksh0521@weaverloft.com>
2025-07-03 11:46:28 +0900

    250703기준 최신(회사)

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
         <div className="top-bar">
           <h1 className="magnetic" data-strength="10" data-strength-text="5">
             <div className="credit">@</div>
-            <div className="code-by"><div className="txt-wrap"><span>Kim Sunhyeong</span><span>Code by</span></div></div>
+            <div className="code-by"><div className="txt-wrap"><span>Code by Kim</span><span>Sunhyeong</span></div></div>
           </h1>
           <ul>
             <li><a href="/" className="magnetic" data-strength="30" data-strength-text="15">Home</a></li>
@@ -88,6 +88,20 @@ function App() {
           </div>
         </div>
         <div className="character"></div>
+        <div className="produce-wrap">
+            <svg width="14px" height="14px" viewBox="0 0 14 14" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <title>arrow-up-right</title>
+              <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                  <g id="Artboard" transform="translate(-1019.000000, -279.000000)" stroke="#111111" strokeWidth="1.5">
+                      <g id="arrow-up-right" transform="translate(1026.000000, 286.000000) rotate(90.000000) translate(-1026.000000, -286.000000) translate(1020.000000, 280.000000)">
+                          <polyline id="Path" points="2.76923077 0 12 0 12 9.23076923"></polyline>
+                          <line x1="12" y1="0" x2="0" y2="12" id="Path"></line>
+                      </g>
+                  </g>
+              </g>
+            </svg>
+            <p>UX<br/>PUBLISHER</p>
+        </div>
       </section>
       <section className="work-sec" style={{height: '100vh'}}>
         <div className="section-inner">
